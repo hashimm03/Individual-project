77ce7a421bce86e3fc15d1f572624b0b024fb83a
Muhammad Hashim <sc21mh2@leeds.ac.uk>
2024-02-13 19:33:46 +0000

    rough class definition for decision trees

--- dt.py
+++ dt.py
@@ -1,23 +1,24 @@
-class DecisionTreeNode:
-    def __init__(self, attribute=None, value=None, true_branch=None, false_branch=None, result=None):
-        self.attribute = attribute  # Attribute to split on
-        self.value = value          # Value of the attribute to split on
-        self.true_branch = true_branch  # Subtree for when the attribute is true
-        self.false_branch = false_branch  # Subtree for when the attribute is false
-        self.result = result        # Result if it's a leaf node
+class TreeNode:
+    def __init__(self, feature=None, value=None, left=None, right=None):
+        self.feature = feature  # Feature associated with the node
+        self.value = value      # 0 or 1 if it's a leaf node, None otherwise
+        self.left = left        # Left child node
+        self.right = right      # Right child node
 
 class DecisionTree:
     def __init__(self, root=None):
-        self.root = root  # Root node of the decision tree
+        self.root = root        # Root node of the decision tree
+    
+    def addNode():
+        pass
 
-    def predict(self, instance):
-        return self._predict(instance, self.root)
-
-    def _predict(self, instance, node):
-        if node.result is not None:
-            return node.result
-
-        if instance[node.attribute] == node.value:
-            return self._predict(instance, node.true_branch)
-        else:
-            return self._predict(instance, node.false_branch)
+def FindStrictExtStr(C, M, e):
+    if (M == None):
+        l0 = TreeNode(value=0)
+        M0 = DecisionTree(root=l0)
+        l1 = TreeNode(value=1)
+        M1 = DecisionTree(root=l1)
+    
+        return M0, M1
+    
+    
