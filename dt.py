f5e6b148840fb4411dd8dd78f6ee72a6be298d84
Muhammad Hashim <sc21mh2@leeds.ac.uk>
2024-02-20 15:39:23 +0000

    created an extra file called config whioch stores C and C features

--- dt.py
+++ dt.py
@@ -1,15 +1,22 @@
+import config
 class TreeNode:
     def __init__(self, feature=None, value=None, child0=None, child1=None):
         self.feature = feature  # Feature associated with the node
         self.value = value      # 0 or 1 if it's a leaf node, None otherwise
         self.child0 = child0      # Left child node
         self.child1 = child1      # Right child node
+        if(self.value != None):
+            self.annotation = getAnnotation(self)
+    
+    def getAnnotation(leaf):
+        annotation = []
+
+        return annotation
 
 class DecisionTree:
     def __init__(self, root=None):
         self.root = root        # Root node of the decision tree
     
-
 def FindStrictExtStr(C, M, e):
     if (M == None):
         l0 = TreeNode(value=0)
@@ -24,7 +31,7 @@ def FindStrictExtStr(C, M, e):
     # ePath is the path to get to this tree
     eLeaf = M.root
     ePath = []
-    ePath.add(eLeaf)
+    ePath.append(eLeaf)
     while(eLeaf.value == None ):
         for f in CFeatures:
             index = 0
@@ -35,10 +42,12 @@ def FindStrictExtStr(C, M, e):
             eLeaf = eLeaf.child0
         else:
             eLeaf = eLeaf.child1
-        ePath.add(eLeaf)
+        ePath.append(eLeaf)
 
     # e_ value of eLeaf
-    e_ = eLeaf.value
+    e_ = eLeaf.examples[0]
+    disagreeFeatures = []
+
 
     # for all features e and e_ diagree on - features that havent been considered in current tree?
         # M_ = extend tree for that feature
--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-C = [[1, 0, 1, 1, 1],
-     [1, 0, 0, 0, 0],
-     [1, 1, 0, 0, 0],
-     [1, 1, 1, 0, 0],
-     [1, 0, 1, 0, 1],
-     [0, 0, 0, 0, 1],
-     [0, 0, 1, 0, 1],
-     [0, 1, 0, 0, 1],
-     [0, 1, 1, 0, 1],]
-CFeatures = ["A", "B", "C", "D"]
+import dt
+import config
 
 def FindOptModelStr(C, s):
     return FindOptExtStr(C, s, None)
@@ -16,13 +8,42 @@ def FindOptExtStr(C, s, M):
     # search examples on tree and see if values are equal
     # return M
 
+    incorrectExample = None
+    countExamples = len(C)
+    countCorrectExamples = 0
+    for e in C:
+        n = M.root
+        while(n.value == None ):
+            for f in CFeatures:
+                index = 0
+                if f == eLeaf.feature:
+                    break
+
+            if e[index] == 0:
+                n = n.child0
+            else:
+                n = n.child1
+        
+        if n.value == e[-1]:
+            countCurrentExamples += 1
+        else:
+            incorrectExample = e
+    
+    if countCurrentExamples == countExamples:
+        return M
+    
     node = M.root
-    count = 0
+    count = 1
     while(node.value == None ):
         count += 1
         node = node.child0
-
+    count += 1
+    while count >= s:
+        return None
     
+
+
+
 
 
 
